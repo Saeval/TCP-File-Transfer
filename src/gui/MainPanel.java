40b4159f1f70eae1d325f58aa7835c6ca69bf278
Saeval <Saeval@users.noreply.github.com>
2016-03-20 10:18:38 +0100

    Now MainPanel has more content!

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -15,9 +15,9 @@ import client.Director;
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel{
 	
-	private static final int LABELS_NUMBER = 4;
-	private static final int FIELDS_NUMBER = 2;
-	private static final int BUTTONS_NUMBER = 3;
+	private static final int LABELS_NUMBER = 5;
+	private static final int FIELDS_NUMBER = 3;
+	private static final int BUTTONS_NUMBER = 4;
 	
 	private Director director = Director.getOnlyDirector();
 	
@@ -26,10 +26,14 @@ public class MainPanel extends JPanel{
 	private JButton[] buttons = new JButton[BUTTONS_NUMBER];
 	
 	public MainPanel() {
-		setLayout(new GridLayout(3, 3));
+		setLayout(new GridLayout(4, 3));
 		generateUIElements();
 		addUIElements();
 		
+		makeTheButtonsMagical();
+	}
+
+	private void makeTheButtonsMagical() {
 		buttons[0].addActionListener(new ActionListener() {
 			
 			@Override
@@ -49,6 +53,14 @@ public class MainPanel extends JPanel{
 		buttons[2].addActionListener(new ActionListener() {
 			
 			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				setIP();
+			}
+		});
+		
+		buttons[3].addActionListener(new ActionListener() {
+			
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				startDownload();
 			}
@@ -62,16 +74,23 @@ public class MainPanel extends JPanel{
 		add(fields[0]);
 		add(buttons[0]);
 		
-		labels[1].setText("Insert destination filename here: ");
+		labels[1].setText("Insert name of file to download here: ");
 		buttons[1].setText("Check");
 		add(labels[1]);
 		add(fields[1]);
 		add(buttons[1]);
 		
-		buttons[2].setText("Connect");
+		labels[2].setText("Insert server IP address here: ");
+		buttons[2].setText("Set IP");
 		add(labels[2]);
-		add(labels[3]);
+		add(fields[2]);
 		add(buttons[2]);
+		
+		buttons[3].setText("Connect");
+		add(labels[3]);
+		add(labels[4]);
+		add(buttons[3]);
+		
 	}
 
 	private void generateUIElements() {
@@ -90,41 +109,52 @@ public class MainPanel extends JPanel{
 		
 		try{
 			if (director.createSocket(tmp[0], Integer.parseInt(tmp[1]))){
-				labels[2].setForeground(Color.GREEN);
-				labels[2].setText("Server is reachable");
+				labels[3].setForeground(Color.GREEN);
+				labels[3].setText("Server is reachable");
 			} else {
-				labels[2].setForeground(Color.RED);
-				labels[2].setText("Could not connect to server");
+				labels[3].setForeground(Color.RED);
+				labels[3].setText("Could not connect to server");
 			}
 		} catch (ArrayIndexOutOfBoundsException e1){
 			fields[0].setText("hostname:port");
-			labels[2].setForeground(Color.YELLOW);
-			labels[2].setText("Please type address and port");
+			labels[3].setForeground(Color.YELLOW);
+			labels[3].setText("Please type address and port");
 		} catch (NumberFormatException e2) {
-			labels[2].setForeground(Color.YELLOW);
-			labels[2].setText("Port has to be a number");
+			labels[3].setForeground(Color.YELLOW);
+			labels[3].setText("Port has to be a number");
 		}
 		
 	}
 	
 	public void startDownload(){
 		if (director.startDownload()){
+			labels[4].setForeground(Color.GREEN);
+			labels[4].setText("Downloading file");
+		} else {
+			labels[4].setForeground(Color.RED);
+			labels[4].setText("Could not download file");
+		}	
+	}
+	
+	public void setIP(){
+		if (director.setIP(fields[2].getText())){
 			labels[3].setForeground(Color.GREEN);
-			labels[3].setText("Downloading file");
+			labels[3].setText("Server IP Address set");
 		} else {
 			labels[3].setForeground(Color.RED);
-			labels[3].setText("Could not download file");
-		}	
+			labels[3].setText("Could not set IP Address");
+		}
+		
 	}
 	
 	public void setFilename(){
 		if (fields[1].getText().isEmpty())
 			fields[1].setText("DownloadedFile");
-//		if (director.setFilename(fields[1].getText())){
+		if (director.setFilename(fields[1].getText())){
 //		if (true){
 			labels[2].setForeground(Color.GREEN);
 			labels[2].setText("Output filename changed");
-//		}
+		}
 	}
 
 }
